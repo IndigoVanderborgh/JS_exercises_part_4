f75e2578541f536843ed711d9c166f5367c6491d
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-26 23:18:58 -0700

    Update JS_exercises_5.js

--- JS_exercises_5.js
+++ JS_exercises_5.js
@@ -11,6 +11,18 @@
 // "The 2 best fruits are Apples, Bananas"
 
 // Use both a for loop and a template string to solve the challenge
+
+// This is the first iteration of the challenge where it returns a comma at the end
+// this is less than intended, so we need to get rid of it
+
+// function generateSentence(desc, arr) {
+//     let baseString = `The ${arr.length} ${desc} are `
+//     for (let i = 0; i < arr.length; i++) {
+//         baseString += arr[i] + ", "
+//     }
+//     return baseString
+// }
+
 function generateSentence(desc, arr) {
     let baseString = `The ${arr.length} ${desc} are ` // this is the base string, what we build on
     const lastIndex = arr.length - 1 // we want to remove the comma at the end so we remove it with line 18's if statement
