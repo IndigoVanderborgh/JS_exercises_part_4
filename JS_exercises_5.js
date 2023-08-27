bf5ac3fa450ccfb560a9787a56ef25c943354fb9
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-26 23:29:16 -0700

    Update JS_exercises_5.js

--- JS_exercises_5.js
+++ JS_exercises_5.js
@@ -13,7 +13,7 @@
 // Use both a for loop and a template string to solve the challenge
 
 // This is the first iteration of the challenge where it returns a comma at the end
-// this is less than intended, so we need to get rid of it
+// this is less than intended, so we need to get rid of that comma
 
 // function generateSentence(desc, arr) {
 //     let baseString = `The ${arr.length} ${desc} are `
@@ -23,14 +23,19 @@
 //     return baseString
 // }
 
+// const sentence = generateSentence("highest mountains", ["Mount Everest", "K2"])
+// console.log(sentence)
+
+// --> "The 2 highest mountains are Mount Everest, K2,"
+
 function generateSentence(desc, arr) {
     let baseString = `The ${arr.length} ${desc} are ` // this is the base string, what we build on
     const lastIndex = arr.length - 1 // we want to remove the comma at the end so we remove it with line 18's if statement
     for (i = 0; i < arr.length; i++) { 
         if (i === lastIndex) {
-            baseString += arr[i] + "," // adds comma after each item, if last index isn't the last one in the array
-        } else {
             baseString += arr[i] // we add this so it doesn't add commas at the end
+        } else {
+            baseString += arr[i] + ", " // adds comma after each item, if last index isn't the last one in the array
         }
     }
     return baseString
