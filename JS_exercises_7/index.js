2d23d718aef98f3c7fdec95426f3f513b2c3b9a9
IndigoVanderborgh <91171405+IndigoVanderborgh@users.noreply.github.com>
2023-08-27 22:09:08 -0700

    Update index.js

--- JS_exercises_7/index.js
+++ JS_exercises_7/index.js
@@ -17,11 +17,25 @@
 // The toFixed() method doesn't work anymore. Can you make it work?
 // Google the error message if you're unsure about how to do it
 
-const totalPrice = 420.69235632455
-const btn = document.getElementById("purchase-btn")
-btn.textContent = `Buy €${ totalPrice.toFixed(2) }`
+// const totalPrice = 420.69235632455
+// const btn = document.getElementById("purchase-btn")
+// btn.textContent = `Buy €${ totalPrice.toFixed(2) }`
 
 // toFixed is not a function; so we need to convert the value to a number before calling the 'toFixed' method or only call the method on numbers
 
 // for example: 
-// const num 
+// const num = '123'
+// const result = num.tofixed(2) --> will throw us an error
+
+// to solve this we can convert the value to a number before calling the tofixed mothod
+
+// const num = '123.456'
+
+// const result = Number(num).tofixed(2)
+// console.log(result) // 123.45
+
+// circling back, lets solve this issue now:
+
+const totalPrice = 420.69235632455
+const btn = document.getElementById("purchase-btn")
+btn.textContent = `Buy €${Number(totalPrice).toFixed(2)}`
